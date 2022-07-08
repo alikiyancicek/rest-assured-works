03131c0feb8c470a168aaeaff227ac18eebc85e2
Ali Kiyancicek <ali.kiyancicek@gmail.com>
2022-07-07 21:59:27 -0400

    first commit

---
+++ src/main/java/ValidateAddPlaceAPI.java
@@ -0,0 +1,12 @@
+public class ValidateAddPlaceAPI {
+    public static void main(String[] args) {
+        // first example - validate if "Add Place API" ia working properly
+
+        //given, when, there methods in the automation tests in rest assured
+
+        //given - all input details
+        //when - submit the API
+        //then - validate the response
+    }
+
+}

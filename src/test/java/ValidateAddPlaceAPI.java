4ace9821253b849313591c7fa7b7be4945248029
Ali Kiyancicek <ali.kiyancicek@gmail.com>
2022-07-09 15:49:13 -0400

    executing and writing response body as string and parsing the response JSON elements

--- src/test/java/ValidateAddPlaceAPI.java
+++ src/test/java/ValidateAddPlaceAPI.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 public class ValidateAddPlaceAPI {
@@ -15,22 +17,31 @@ public class ValidateAddPlaceAPI {
         // given belong to rest assured static package so we need to import that static package. ln 2
         // following line , we describe our key ( from url ) and its value ( refer to postman)
         //.log().all() shows givin data as well as output perfectly. this is a must
-        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+        String response = given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
                 // calling clas-> then the static method which has our json data
                 .body(jsonData.jsonDatas()).
                 when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200)
+                .then().assertThat().statusCode(200)
                 //validating if scope=APP with body and equal to method. also equal to method is under the static package called harmcast.matchers.
                 // we need to import it manually
                 .body("scope",equalTo("APP"))
                 // all methods after written THEN will be return or output.
                 // following line checks the server is validated as ours or not. because we might hacked.
                 //this is most commonly used technic
-                .header("server","Apache/2.4.41 (Ubuntu)");
+                .header("server","Apache/2.4.41 (Ubuntu)").extract().response().asString();
 
                 // Add place - > Update place with new address -> get place to validate if new address presents in the response.
                 // ( end to end automation test sample)
                 // first need to tell which place going to change based on place_id
+                // line 29 + line 18 added response variable and >---extracting response as a string
+        System.out.println(response);
+
+        // now want to parse the JSON response using Json Path class
+        JsonPath js = new JsonPath(response);
+        //path place_id has direct access . if it was nested , we should provide full path
+        String placeId = js.getString("place_id");
+        //now we extracted place placeId
+        System.out.println(placeId);
 
     }
 
